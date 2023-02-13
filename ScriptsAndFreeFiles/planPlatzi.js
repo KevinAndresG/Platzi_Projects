d794a97353018b1dbdaf20867e617695b13cf9fb
Kevin Garcia <kevin.garcia@sinapsis.global>
2023-02-13 13:04:23 -0500

    new

--- ScriptsAndFreeFiles/planPlatzi.js
+++ ScriptsAndFreeFiles/planPlatzi.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 function YourPlan(User) {
-  let my_dict = {
-    free: "Solo puedes tomar los cursos gratis",
-    basic: "Puedes tomar casi todos los cursos de Platzi durante un mes",
-    expert: "Puedes tomar casi todos los cursos de Platzi durante un año",
-    expert_plus: "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"
-  }
-  const final = User.replace(" ", "_").toLowerCase()
-  if (final in my_dict) {
-    console.log(my_dict[final])
-  }
+    let my_dict = {
+        free: "Solo puedes tomar los cursos gratis",
+        basic: "Puedes tomar casi todos los cursos de Platzi durante un mes",
+        expert: "Puedes tomar casi todos los cursos de Platzi durante un año",
+        expert_plus: "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"
+    }
+    const final = User.replace(" ", "_").toLowerCase()
+    if (final in my_dict) {
+        console.log(my_dict[final])
+    } else {
+      console.log("Opcion Inválida")
+    }
 }
 YourPlan("Expert Plus")
